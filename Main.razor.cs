7fca24a15858a7283cafa0fe91db8767004ab371
HeathAndrew <andrew.heath@magstim.com>
2023-11-02 13:40:13 +0000

    added "drawing cache" to demonstrate obsreved behaviour

--- Main.razor.cs
+++ Main.razor.cs
@@ -119,27 +119,13 @@ namespace BlazorTestApp
         {
             if (ChartData.Count != 0 && Chart is not null)
             {
+                string newID = AnnoIDAmp + rand.Next();
                 AmpOrderedData = (from v in ChartData orderby v.yValue descending select v.yValue).ToList();
-
-
                 IndexOfAmp1Channel1 = 0;
-
-
-
-
                 Amp1Channel1 = (float)Math.Round(AmpOrderedData[IndexOfAmp1Channel1], 5, MidpointRounding.AwayFromZero);
-
-
-
-                Amp1Channel1Annotation = NewAnnotationYAxis("Channel1Amp1", Math.Round(AmpOrderedData[IndexOfAmp1Channel1], 2, MidpointRounding.AwayFromZero), "Amp 1");
-
-
-
-
+                Amp1Channel1Annotation = NewAnnotationYAxis(newID, Math.Round(AmpOrderedData[IndexOfAmp1Channel1], 2, MidpointRounding.AwayFromZero), "Amp 1");
                 await Chart.AddYAxisAnnotationAsync(Amp1Channel1Annotation, false);
-
-
-
+                InitCache(DrawingCacheAmp, newID);
             }
         }
         public async Task InitCursorsXCursors()
@@ -148,56 +134,89 @@ namespace BlazorTestApp
             {
                 int startIndex = 800;
                 int endIndex = (ChartData.Count / 4) * 3;
-
-                Time1Channel1Annotation = NewAnnotationXAxis("Channel1Time1", startIndex, "T1");
-                Time2Channel1Annotation = NewAnnotationXAxis("Channel1Time2", endIndex, "T2");
-
-
+                string NewID = AnnoIDTime1 + rand.Next();
+                string NewID2 = AnnoIDTime2 + rand.Next();
+                Time1Channel1Annotation = NewAnnotationXAxis(NewID, startIndex, "T1");
+                Time2Channel1Annotation = NewAnnotationXAxis(NewID2, endIndex, "T2");
                 Time1Channel1 = startIndex;
                 Time2Channel1 = endIndex;
-
                 await Chart.AddXAxisAnnotationAsync(Time1Channel1Annotation, false);
                 await Chart.AddXAxisAnnotationAsync(Time2Channel1Annotation, false);
+                InitCache(DrawingCacheTime1, NewID);
+                InitCache(DrawingCacheTime2, NewID2);
 
             }
         }
+
+        string AnnoIDAmp = "Amp";
+        string AnnoIDTime1 = "Time1";
+        string AnnoIDTime2 = "Time2";
+
+        Random rand = new Random();
+        string[] DrawingCacheAmp = new string[2];
+        string[] DrawingCacheTime2 = new string[2];
+        string[] DrawingCacheTime1 = new string[2];
         public async Task CursorAdjust(int AdjustAmount)
         {
             if (Chart is not null)
             {
+                string NewTimeOneID = AnnoIDTime1 + rand.Next();
+                string NewTimeTwoID = AnnoIDTime2 + rand.Next();
+                string NewAmpID = AnnoIDAmp + rand.Next();
                 switch (SelectedChannel)
                 {
                     case Channels.None: break;
                     case Channels.Time1Channel1:
-                        await Chart.RemoveAnnotationAsync("Channel1Time1");
-                        await Chart.RemoveAnnotationAsync("Channel1Time2");
+
                         Time1Channel1 += AdjustAmount;
-                        Time1Channel1 = Math.Clamp(Time1Channel1, 0, ChartData.Count - 1);
-                        Time1Channel1Annotation = NewAnnotationXAxis("Channel1Time1", (int)Time1Channel1, "T1");
-                        Time2Channel1Annotation = NewAnnotationXAxis("Channel1Time2", (int)Time2Channel1, "T2");
+                        Time1Channel1 += Math.Clamp(Time1Channel1, 0, ChartData.Count - 1);
+
+                        Time1Channel1Annotation = NewAnnotationXAxis(NewTimeOneID, (int)Time1Channel1, "T1");
+                        Time2Channel1Annotation = NewAnnotationXAxis(NewTimeTwoID, (int)Time2Channel1, "T2");
+
                         await Chart.AddXAxisAnnotationAsync(Time1Channel1Annotation, false);
                         await Chart.AddXAxisAnnotationAsync(Time2Channel1Annotation, false);
+
+                        UpdateCache(DrawingCacheTime1, NewTimeOneID);
+                        UpdateCache(DrawingCacheTime2, NewTimeTwoID);
+
+                        await Chart.RemoveAnnotationAsync(DrawingCacheTime1[1]);
+                        await Chart.RemoveAnnotationAsync(DrawingCacheTime2[1]);
                         break;
                     case Channels.Time2Channel1:
-                        await Chart.RemoveAnnotationAsync("Channel1Time1");
-                        await Chart.RemoveAnnotationAsync("Channel1Time2");
+                        
                         Time2Channel1 += AdjustAmount;
-                        Time2Channel1 = Math.Clamp(Time2Channel1, 0, ChartData.Count - 1);
-                        Time1Channel1Annotation = NewAnnotationXAxis("Channel1Time1", (int)Time1Channel1, "T1");
-                        Time2Channel1Annotation = NewAnnotationXAxis("Channel1Time2", (int)Time2Channel1, "T2");
+                        Time2Channel1 = Math.Clamp(AdjustAmount, 0, ChartData.Count - 1);
+                        Time1Channel1Annotation = NewAnnotationXAxis(NewTimeOneID, (int)Time1Channel1, "T1");
+                        Time2Channel1Annotation = NewAnnotationXAxis(NewTimeTwoID, (int)Time2Channel1, "T2");
                         await Chart.AddXAxisAnnotationAsync(Time1Channel1Annotation, false);
                         await Chart.AddXAxisAnnotationAsync(Time2Channel1Annotation, false);
+                        UpdateCache(DrawingCacheTime1, NewTimeOneID);
+                        UpdateCache(DrawingCacheTime2, NewTimeTwoID);
+                        await Chart.RemoveAnnotationAsync(DrawingCacheTime1[1]);
+                        await Chart.RemoveAnnotationAsync(DrawingCacheTime2[1]);
                         break;
                     case Channels.Amp1Channel1:
-                        await Chart.RemoveAnnotationAsync("Channel1Amp1");
                         IndexOfAmp1Channel1 += AdjustAmount;
                         IndexOfAmp1Channel1 = Math.Clamp(IndexOfAmp1Channel1, 0, ChartData.Count - 1);
+
                         Amp1Channel1 = (float)Math.Round(AmpOrderedData[IndexOfAmp1Channel1], 5, MidpointRounding.AwayFromZero);
-                        Amp1Channel1Annotation = NewAnnotationYAxis("Channel1Amp1", Math.Round(AmpOrderedData[IndexOfAmp1Channel1], 2, MidpointRounding.AwayFromZero), "Amp 1");
+                        Amp1Channel1Annotation = NewAnnotationYAxis(NewAmpID, Math.Round(AmpOrderedData[IndexOfAmp1Channel1], 2, MidpointRounding.AwayFromZero), "Amp 1");
                         await Chart.AddYAxisAnnotationAsync(Amp1Channel1Annotation, false);
+                        UpdateCache(DrawingCacheAmp, NewAmpID);
+                        await Chart.RemoveAnnotationAsync(DrawingCacheAmp[1]);
                         break;
                 }
             }
+        }
+        public void InitCache(string[] cache, string newValue)
+        {
+            cache[0] = newValue;
+        }
+        public void UpdateCache(string[] cache, string newValue)
+        {
+            cache[1] = cache[0];
+            cache[0] = newValue;
         }
         public ApexChartOptions<Data> ReturnChartOptions(string ID)
         {
